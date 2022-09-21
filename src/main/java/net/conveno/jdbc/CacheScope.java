950efe0e5cb006925e9d4e585a55ef4f8b2d6199
itzstonlex <seltixfromhell@bk.ru>
2022-09-21 13:07:33 +0400

    build repository updates to JAR

--- src/main/java/net/conveno/jdbc/CacheScope.java
+++ src/main/java/net/conveno/jdbc/CacheScope.java
@@ -1,5 +1,6 @@
 package net.conveno.jdbc;
 
+import lombok.SneakyThrows;
 import net.conveno.jdbc.proxied.ProxiedConnection;
 import net.conveno.jdbc.proxied.ProxiedQuery;
 
@@ -17,9 +18,10 @@ public enum CacheScope {
 
     PROTOTYPE {
 
+        @SneakyThrows
         @Override
         public ProxiedQuery processGet(ProxiedConnection connection, String sql, Map<String, ProxiedQuery> map) {
-            return map.computeIfAbsent(sql, __ -> new ProxiedQuery(connection, sql)).clone();
+            return CacheScope.SINGLETON.processGet(connection, sql, map).clone();
         }
     },
     ;
